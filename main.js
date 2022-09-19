f6bea0405e6bbc52f52b601f4d2750839284ad4b
larisaoprea1 <103680895+larisaoprea1@users.noreply.github.com>
2022-09-19 15:42:27 +0300

    creating vue app, v-if, v-for, v-on understandings

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return{
+            cart:0,
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.nike.com/ro/w/mens-socks-7ny3qznik1',
+            inventory: 9,
+            onSale: true, 
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:2234, color:'green',size: '22.35cm'},
+                {id:2235, color:'blue',size:'22.85cm'}
+            ],
+            
+        }
+    }
+})
