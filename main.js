2ec6da8b377265760356125ef21a0e172bda068e
larisaoprea1 <103680895+larisaoprea1@users.noreply.github.com>
2022-09-19 16:50:54 +0300

    event handling

--- main.js
+++ main.js
@@ -5,14 +5,26 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             url: 'https://www.nike.com/ro/w/mens-socks-7ny3qznik1',
-            inventory: 9,
-            onSale: true, 
+            inventory: 0,
+            inStock: true,
+            onSale: false, 
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id:2234, color:'green',size: '22.35cm'},
-                {id:2235, color:'blue',size:'22.85cm'}
+                {id:2234, color:'green',size: '22.35cm', image: './assets/images/socks_green.jpg'},
+                {id:2235, color:'blue',size:'22.85cm', image: './assets/images/socks_blue.jpg'}
             ],
             
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart +=1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        removeFromCart(){
+            this.cart -=1
         }
     }
 })
