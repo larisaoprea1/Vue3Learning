24f5b21877418eafc327900c7e844b17c1beb805
larisaoprea1 <103680895+larisaoprea1@users.noreply.github.com>
2022-09-20 09:59:15 +0300

    Computed properties

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const app = Vue.createApp({
         return{
             cart:0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
             url: 'https://www.nike.com/ro/w/mens-socks-7ny3qznik1',
             inventory: 0,
-            inStock: true,
             onSale: false, 
+            brand: 'Vue Mastery',
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id:2234, color:'green',size: '22.35cm', image: './assets/images/socks_green.jpg'},
-                {id:2235, color:'blue',size:'22.85cm', image: './assets/images/socks_blue.jpg'}
+                {id:2234, color:'green',size: '22.35cm', image: './assets/images/socks_green.jpg',quantity: 50, isOnStock: true },
+                {id:2235, color:'blue',size:'22.85cm', image: './assets/images/socks_blue.jpg', quantity: 0, isOnStock: false}
             ],
             
         }
@@ -20,11 +20,35 @@ const app = Vue.createApp({
         addToCart(){
             this.cart +=1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
+            console.log(index);
         },
         removeFromCart(){
             this.cart -=1
+        }
+    },
+    computed:{
+        title(){
+            return this.brand +" "+ this.product;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        stock(){
+            if(this.variants[this.selectedVariant].isOnStock){
+                return 'On Stock'
+            }
+            else return 'Out of Stock'
+        },
+        sale(){
+            if(this.onSale){
+                return this.title + ' ' + 'is on sale'
+            }
+            else return this.title
         }
     }
 })
